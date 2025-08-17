7803915522dec0f51f944f42b13284fa5761b231
Himanshu rana <137643570+himanshurana18@users.noreply.github.com>
2025-08-17 16:01:42 +0530

    Fix navbar.scss import case sensitivity

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import "./navbar.scss";
+import "./Navbar.scss";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { useNotificationStore } from "../../lib/notificationStore";
